a1e91e7c7e0ffc92bd1af52ea7e1549db3bd8b2f
5eo1ab <myhb9188@seoultech.ac.kr>
2018-06-20 03:08:38 +0900

    script summary data size RE

--- script_summary_file.py
+++ script_summary_file.py
@@ -12,12 +12,12 @@ for item in list_dir:
 	item_path = './temp_file/{}'.format(item)
 	if os.path.isfile(item_path):
 		f_format, f_size = item.split('.')[-1], os.path.getsize(item_path)
-		res_list.append((f_format, item, f_size))
+		res_list.append((f_format, item, f_size, 1))
 	else: 
 		f_size = sum(os.path.getsize('{}/{}'.format(item_path,f)) for f in os.listdir(item_path))
-		res_list.append(('dir', item, f_size))
+		res_list.append(('dir', item, f_size, len(os.listdir(item_path))))
 
-res_df = df(res_list, columns=['TYPE', 'NAME', 'SIZE(bytes)'])
+res_df = df(res_list, columns=['TYPE', 'NAME', 'SIZE(bytes)', 'COUNT_FILES'])
 res_df.to_csv('./metadata_zip_file/summary_collect_data.csv', index=False)
 print(res_df.head())
 
